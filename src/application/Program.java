b658fbdfff4edafc12ca9f31748875d808fdc4e6
Teillon Duarte <teillon.duarte@outlook.com>
2026-09-02 11:07:50 -0300

    SellerDao and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +15,8 @@ public class Program {
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 	}
 
